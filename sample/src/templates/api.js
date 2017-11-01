ef6b033de9faed9b7f290b62f5d6a7aeabfeb36a
devdigital <devdigital.cloud@gmail.com>
2017-11-01 14:30:31 +0000

    chore: adds verb colors

--- sample/src/templates/api.js
+++ sample/src/templates/api.js
@@ -4,24 +4,99 @@ import remark from 'remark'
 import reactRenderer from 'remark-react'
 import groupBy from 'lodash.groupby'
 
+const SpecInformation = ({ title, version, description }) => (
+  <div>
+    <h1>{title}</h1>
+    <p>{version}</p>
+    <div>
+      {
+        remark()
+          .use(reactRenderer)
+          .processSync(description).contents
+      }
+    </div>
+  </div>
+)
+
+SpecInformation.propTypes = {
+  title: PropTypes.string.isRequired,
+  version: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+}
+
+const Verb = ({ value, style }) => {
+  let color = null
+
+  switch (value) {
+    case 'get':
+      color = '#61affe'
+      break
+    case 'post':
+      color = '#49cc90'
+      break
+    case 'put':
+      color = '#fca130'
+      break
+    case 'delete':
+      color = '#f93e3e'
+      break
+    default:
+      color = '#fff'
+      break
+  }
+
+  const verbStyle = {
+    padding: '0.2rem 0.5rem',
+    backgroundColor: color,
+    color: '#fff',
+  }
+
+  return <p style={Object.assign(verbStyle, style)}>{value.toUpperCase()}</p>
+}
+
+const SpecPath = ({ path }) => (
+  <div>
+    <div style={{ display: 'flex' }}>
+      <Verb style={{ marginRight: '1rem' }} value={path.verb} />
+      <p style={{ fontWeight: 600 }}>{path.name}</p>
+      <p style={{ marginLeft: 'auto' }}>{path.summary}</p>
+    </div>
+    {path.description && <p>{path.description}</p>}
+  </div>
+)
+
+SpecPath.propTypes = {
+  path: PropTypes.object.isRequired,
+}
+
+const SpecPaths = ({ tag, paths }) => (
+  <div>
+    <h2>{tag}</h2>
+    {paths.map(p => <SpecPath key={`${p.name}-${p.verb}`} path={p} />)}
+  </div>
+)
+
+SpecPaths.propTypes = {
+  tag: PropTypes.string.isRequired,
+  paths: PropTypes.array.isRequired,
+}
+
 class Api extends Component {
   render() {
     const api = this.props.data.openApiSpec
     const paths = api.childrenOpenApiSpecPath
     const pathGroups = groupBy(paths, p => p.tag)
 
-    console.log(pathGroups)
     return (
       <div>
-        <h1>{api.title}</h1>
-        <p>{api.version}</p>
-        <div>
-          {
-            remark()
-              .use(reactRenderer)
-              .processSync(api.description).contents
-          }
-        </div>
+        <SpecInformation
+          title={api.title}
+          version={api.version}
+          description={api.description}
+        />
+        {Object.keys(pathGroups).map(t => (
+          <SpecPaths key={t} tag={t} paths={pathGroups[t]} />
+        ))}
       </div>
     )
   }
